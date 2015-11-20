6d9ccb623287055fcf00c77423789b9d18ae85fc
Timothy Perrett <timothy.m.perrett@one.verizon.com>
2015-11-20 15:37:20 -0800

    looks to be working?

--- flask/entrypoint.sh
+++ flask/entrypoint.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ash
 
 echo "waiting for elasticsearch"
 while true; do
-    nc -q 1 elasticsearch 9200 2>/dev/null && break
+  nc -n -i 1 elasticsearch 9200 2>/dev/null && break
 done
 
-echo "starting flask..."
-java -jar \
+echo "starting flask:"
+/usr/bin/java -jar \
   /usr/share/oncue/flask.jar \
-  -Dlogback.configurationFile=${{chdir}}/etc/logback.xml
+  -Dlogback.configurationFile=/usr/share/oncue/etc/logback.xml \
   -XX:+UseStringDeduplication \
   -XX:+UseG1GC \
   -XX:MaxGCPauseMillis=600 \
